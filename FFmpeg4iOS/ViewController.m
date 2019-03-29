5e0d0f99be399fcb4526e0469a525c9887b797af
obarong <dustinljr@gmail.com>
2019-03-29 15:05:40 +0800

    添加ffmpeg、h264源码和编译脚本 添加a文件和头文件，未集成h264 实现RTSP转mp4

--- FFmpeg4iOS/ViewController.m
+++ FFmpeg4iOS/ViewController.m
@@ -7,7 +7,19 @@
 //
 
 #import "ViewController.h"
-
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include "libavutil/opt.h"
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "ffmpeg.h"
+    
+#ifdef __cplusplus
+};
+#endif
 @interface ViewController ()
 
 @end
@@ -16,8 +28,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    av_register_all();
+//    NSLog(@"version=%d\n", avcodec_version());
+    
+    //获取文件位置
+    NSString *docPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+//    NSString *string =@"ffmpeg -i rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov -vcodec copy -t 5 ";
+    NSString *vidPath = [NSString stringWithFormat:@"%@/testfile.mpg", docPath];
+    NSString *outPath = [NSString stringWithFormat:@"%@/out.mp4", docPath];
+    NSString *cmd=[NSString stringWithFormat:@"ffmpeg -y -i rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov -c:v copy -t 5 %@", outPath];
+//    NSString *cmd=[NSString stringWithFormat:@"ffmpeg -y -i %@ -c:v copy -t 5 %@", vidPath, outPath];
+    NSArray *array = [cmd componentsSeparatedByString:@" "];
+    NSLog(@"array:%@",array);
+    
+    int argc = [array count];
+    char *argv[argc];
+    for (int i = 0; i < argc; i++) {
+        argv[i] = [[array objectAtIndex:i] UTF8String];
+    }
+    jrRun(argc, argv);
+    
 }
-
 
 @end
